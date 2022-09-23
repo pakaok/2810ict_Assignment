795a9c7190a67cbf391223023ba0d05d1b4907a9
pakaok <jaeseok.kim@griffithuni.edu.au>
2022-09-23 19:08:18 +1000

    GUI updated

--- group98.py
+++ group98.py
@@ -73,8 +73,53 @@ analyze_option3= Radiobutton(root,text='three',value=3,variable=option_val)
 analyze_option4= Radiobutton(root,text='four',value=4,variable=option_val)
 analyze_option5= Radiobutton(root,text='fifth',value=5,variable=option_val)  
 
+search_text= Entry(root,width=30)
+search_text.place(x=xpos+600,y=ypos+option_ypos*2-option_ypos_1+5)
+
 analyze_option1.place(x=xpos+325,y=ypos-option_ypos_1)
 analyze_option2.place(x=xpos+325,y=ypos+option_ypos-option_ypos_1)
 analyze_option3.place(x=xpos+325,y=ypos+option_ypos*2-option_ypos_1)
 analyze_option4.place(x=xpos+325,y=ypos+option_ypos*3-option_ypos_1)
 analyze_option5.place(x=xpos+325,y=ypos+option_ypos*4-option_ypos_1)   
+
+speedzone=['30km/hr', '40 km/hr','50 km/hr', '60 km/hr','70 km/hr','75 km/hr','80 km/hr', '90 km/hr',
+           '100 km/hr','110 km/hr','Other speed limit','Camping grounds or off road' , 'Not known']
+speedzone.sort()
+speed_comboBox= ttk.Combobox(root,height=len(speedzone),width=15, values=speedzone,state='readonly')
+speed_comboBox.set('Speed Select')
+speed_comboBox.place(x=xpos+600,y=ypos+option_ypos*4-option_ypos_1)
+
+
+tree= ttk.Treeview(root,columns=[i for i in range(1,12)],show='headings',height=19)
+tree.column(1,width=95,anchor='center')
+tree.column(2,width=70,anchor='center')
+tree.column(3,width=50,anchor='center')
+tree.column(4,width=85,anchor='center')
+tree.column(5,width=180,anchor='center')
+tree.column(6,width=80,anchor='center')
+tree.column(7,width=120,anchor='center')
+tree.column(8,width=120,anchor='center')
+tree.column(9,width=90,anchor='center')
+tree.column(10,width=80,anchor='center')
+tree.column(11,width=80,anchor='center')
+
+tree.heading(1, text='Accident_No')
+tree.heading(2, text='Date')
+tree.heading(3, text='Time')
+tree.heading(4, text='Alcohol_Time')
+tree.heading(5, text='Accident_Type')
+tree.heading(6, text='Day_of_Week')
+tree.heading(7, text='Light_Condition')
+tree.heading(8, text='Road_Geometry')
+tree.heading(9, text='Severity')
+tree.heading(10, text='Speed_Zone')
+tree.heading(11, text='Run_Offroad')
+tree.tag_configure('even', background='#E8E8E8')
+
+treeScroll = ttk.Scrollbar(root)
+treeScroll.configure(command=tree.yview)
+tree.configure(yscrollcommand=treeScroll.set)
+treeScroll.place(x=int(wid)-20,y=25,height=410)
+
+tree.place(x=10,y=25)
+root.mainloop()
