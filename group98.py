841c1d2d3b2d4dc381cdbe84dde770e7e868defd
pakaok <jaeseok.kim@griffithuni.edu.au>
2022-10-02 18:43:28 +1000

    Design Doc changed a bit, search by date and chart visualising in py file

--- group98.py
+++ group98.py
@@ -3,7 +3,13 @@ import pandas as pd
 from tkinter import *
 from tkinter import ttk
 import matplotlib.pyplot as plt
-df = pd.read_csv('Crash Statistics Victoria.csv')
+def readCsv(file):
+    try:
+        return pd.read_csv(file)
+    except:
+        return False
+
+df = readCsv('Crash Statistics Victoria.csv')
 
 wid='1075'
 hgt='600'
@@ -12,15 +18,83 @@ lst_month=[i for i in range(1,13)]
 lst_year=[i for i in range(2013,2020)]
 date_list=[]
 
+for i in df.ACCIDENT_DATE:
+    i = i.split('/')
+    if int(i[0])<10:
+        i[0]='0'+i[0]
+    date_list.append(i[2]+i[1]+i[0])
+    
+def date_range(y0,m0,d0,y1,m1,d1):
+    return y0+m0+d0,y1+m1+d1
+
 def btn1():
-    print('hello')
+    tree.delete(*tree.get_children())
+    date_set= date_range(year_comboBox_0.get(),month_comboBox_0.get(),day_comboBox_0.get(),
+    year_comboBox_1.get(),month_comboBox_1.get(),day_comboBox_1.get())
+
+    if option_val.get() ==1 : searchBy_date(date_set)
+    if option_val.get() ==2 : opt2(date_set,day_comboBox_0.get()+'/'+month_comboBox_0.get()+'/'+year_comboBox_0.get()+' To '
+              +day_comboBox_1.get()+'/'+month_comboBox_1.get()+'/'+ year_comboBox_1.get())
+    if option_val.get() ==3 : print('n')
+    if option_val.get() ==4 : print('n')
+    if option_val.get() ==5 : print('n')
+    
+def opt2(date,title):
+    try:
+        start,end=date
+        acc_num=[0 for i in range(0,24)]
+        total_day=0
+        max_value=0
+        for i in range(len(date_list)):
+            if int(date_list[i])>=int(start) and int(date_list[i])<=int(end) :
+                total_day+=1
+                acc_time=df.ACCIDENT_TIME[i].split('.')[0]
+                acc_num[int(acc_time)] = acc_num[int(acc_time)] + 1
+        max_value=max(acc_num)
+        for i in range(0,len(acc_num)):
+            acc_num[i]=acc_num[i]/total_day
+        plt.bar([i for i in range(0,24)], acc_num)
+        plt.xticks([i for i in range(0,24)])
+        plt.yticks([i/total_day for i in range(0,max_value,(max_value)//5)])
+        plt.xlabel('0-24h')
+        plt.ylabel('Number of accident on average')
+        plt.title(title)
+        plt.tight_layout()
+        plt.show()  
+        return True
+    except:
+        return False
+
+def searchBy_date(date):
+    try:
+        start,end=date
+        for i in range(len(date_list)):
+            if int(date_list[i])>=int(start) and int(date_list[i])<=int(end) :
+                acc_time=df.ACCIDENT_TIME[i].split('.')
+                acc_time = acc_time[0]+':'+acc_time[1]
+                injury = df.SEVERITY[i].split()
+                injury = injury[0]+' '+injury[1]
+                if i%2 == 0: tagName='even' 
+                else: tagName = ''
+                tree.insert('','end',values=[
+                    df.ACCIDENT_NO[i],
+                    df.ACCIDENT_DATE[i],
+                    acc_time,
+                    df.ALCOHOLTIME[i],
+                    df.ACCIDENT_TYPE[i],
+                    df.DAY_OF_WEEK[i],
+                    df.LIGHT_CONDITION[i],
+                    df.ROAD_GEOMETRY[i],
+                    injury,
+                    df.SPEED_ZONE[i],
+                    df.RUN_OFFROAD[i]],tag= tagName)
+        return True
+    except: 
+        return False
             
 root = Tk()
-root.title('2810ICT/Kim/Mausham/Sam')
+root.title('2810ICT/Group 98/Kim/Mausham/Sam')
 root.geometry(wid+'x'+hgt+'+170+50')
-# root_style=ttk.Style()
-# root_style.theme_use('alt')
-# root.resizable(False,False)
 btn1 = Button(root,width=10,height=8,text='start',command=btn1)
 xpos=20
 ypos=500
@@ -66,21 +140,21 @@ lb6.place(x=xpos+395/2,y=ypos+combobox_ypos-15,anchor=CENTER)
 option_ypos=25
 option_ypos_1=50
 option_val=IntVar()
-analyze_option1= Radiobutton(root,text='one',value=1,variable=option_val)
+analyze_option1= Radiobutton(root,text='Search by selected period',value=1,variable=option_val)
 analyze_option1.select()
-analyze_option2= Radiobutton(root,text='two',value=2,variable=option_val)
-analyze_option3= Radiobutton(root,text='three',value=3,variable=option_val)
-analyze_option4= Radiobutton(root,text='four',value=4,variable=option_val)
-analyze_option5= Radiobutton(root,text='fifth',value=5,variable=option_val)  
-
-search_text= Entry(root,width=30)
-search_text.place(x=xpos+600,y=ypos+option_ypos*2-option_ypos_1+5)
+analyze_option2= Radiobutton(root,text='Visualise the number of accidents on average',value=2,variable=option_val)
+analyze_option3= Radiobutton(root,text='Search by selected period and accident type',value=3,variable=option_val)
+analyze_option4= Radiobutton(root,text='Visualise the impact of alcohol in accidents',value=4,variable=option_val)
+analyze_option5= Radiobutton(root,text='Search by selected period and speed zone',value=5,variable=option_val)  
 
 analyze_option1.place(x=xpos+325,y=ypos-option_ypos_1)
 analyze_option2.place(x=xpos+325,y=ypos+option_ypos-option_ypos_1)
 analyze_option3.place(x=xpos+325,y=ypos+option_ypos*2-option_ypos_1)
 analyze_option4.place(x=xpos+325,y=ypos+option_ypos*3-option_ypos_1)
 analyze_option5.place(x=xpos+325,y=ypos+option_ypos*4-option_ypos_1)   
+
+search_text= Entry(root,width=30)
+search_text.place(x=xpos+600,y=ypos+option_ypos*2-option_ypos_1+5)
 
 speedzone=['30km/hr', '40 km/hr','50 km/hr', '60 km/hr','70 km/hr','75 km/hr','80 km/hr', '90 km/hr',
            '100 km/hr','110 km/hr','Other speed limit','Camping grounds or off road' , 'Not known']
